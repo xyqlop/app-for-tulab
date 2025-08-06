30e859cf47d1788593feda14750098d6a8163272
xyqlop <akunsampingan31@gmail.com>
2025-08-06 14:16:05 +0800

    feat(HomePage.tsx): add table

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,18 @@ const HomePage = () => {
   return (
     <div>
       <h1>Ini Home Page</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Nama</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>joni</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
